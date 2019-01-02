5a9f3eb603c8d81560971ef9e5595ad4cef18dea
PandawanFr <pandawanfr@gmail.com>
2019-01-01 18:19:34 -0800

    Added Heroku and WIP messenger webhooks

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
-const app = express();
-const port = 3000;
+const bodyParser = require('body-parser');
+const verifyWebhook = require('./src/verify-webhook');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get('/', verifyWebhook);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
